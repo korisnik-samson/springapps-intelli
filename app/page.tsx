8197226c7032deda215c3f18fcd38a766e16e56b
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2024-10-30 11:12:29 +0100

    Modified ShadCN Components

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
     return (
-        <div>
-            <Button>Click me</Button>
+        <div className='flex gap-4'>
+            <Button variant='default'>Primary</Button>
+            <Button variant='secondary'>Secondary</Button>
+            <Button variant='destructive'>Destructive</Button>
+            <Button variant='ghost'>Ghost</Button>
+            <Button variant='outline'>Outline</Button>
+            <Button variant='muted'>Muted</Button>
+            <Button variant='tertiary'>Tertiary</Button>
         </div>
     );
 }
