e8cb6a7a072fe2cf1639ad8d627185a9c8f09edc
Samson <126519078+korisnik-samson@users.noreply.github.com>
2025-04-18 22:13:00 +0200

    Update actions.ts

--- features/auth/actions.ts
+++ features/auth/actions.ts
@@ -1,21 +1,40 @@
 'use server';
 
 import { cookies } from "next/headers";
-import { verify } from "jsonwebtoken";
-import { UserObject } from "@/types";
 import { KeyVaultSecret } from "@azure/keyvault-secrets";
 import { getSecret } from "@/lib/azure-secrets";
+import { verify } from "jsonwebtoken";
+import { getUserFromUsername } from "@/lib/spring";
 
 export const getCurrentUser = async () => {
     try {
         const token = cookies().get('JWT_Token')?.value;
         if (!token) return null;
-        // secret key from Azure might be needed too
-        // verify and decode the token to get the username and validity
-        // if valid then get the user
+        
+        // get Secret from Azure Key Vault
+        const jwtSecret: KeyVaultSecret = await getSecret(process.env.JWT_SECRET_NAME!);
+        console.log({ jwtSecret });
 
-        // const user = getUserByUsername(username, token)
-        // return the user
+        if (!jwtSecret.value) {
+            console.error(`Secret ${process.env.JWT_SECRET_NAME} retrieved but has no value.`);
+            return null;
+        }
+        
+        // verify and decode the token to get the username and validity
+        const decodedToken = verify(
+            token,
+            Buffer.from(jwtSecret.value, 'base64'), {
+            algorithms: ['HS256']
+        });
+
+        const user = getUserFromUsername(decodedToken.sub, token)
+        
+        if (!user) {
+            console.error('User not found');
+            return null;
+        }
+        
+        return user;
         
     } catch (error) {
         console.error('Error fetching current user:', error);
